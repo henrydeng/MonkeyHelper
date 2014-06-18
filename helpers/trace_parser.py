a372f7ae051221df51127caab1a79bfbd7ed0377
Mingyuan Xia <ken.mingyuan@gmail.com>
2014-06-18 16:48:31 -0700

    pipelined version

--- helpers/trace_parser.py
+++ helpers/trace_parser.py
@@ -4,14 +4,38 @@ import sys, re
 # the doc for the MT protocol can be found here:
 # https://www.kernel.org/doc/Documentation/input/multi-touch-protocol.txt
 
+# The parcel exchanged between different steps of a pipeline
+class PipelineParcel:
+    def __init__(self):
+        self.q = []
+    def enqueue(self, obj):
+        self.q.append(obj)
+    def dequeue(self):
+        return self.q.pop(0)
+    def isEmpty(self):
+        return len(self.q) == 0
 
-# A type A multi-touch screen
-class MultiTouchTypeAParser:
-    pass # TODO
+class MotionEvent:
+    timestamp = 0
+    tracking_id = 0xFFFFFFFF
+    touch_major = 0
+    x = 0
+    y = 0
+    pressure = 0
+    def __str__(self):
+        return str((self.timestamp, self.tracking_id, self.touch_major, self.x, self.y, self.pressure))
+
+class GeteventCommand:
+    timestamp = 0
+    evType = ""
+    evCmd = ""
+    evVal = 0
+    def __str__(self):
+        return str((self.timestamp, self.evType, self.evCmd, self.evVal))
 
 # A type B multi-touch screen
 # a list of supported features:
-# MT_PRESSURE, MT_POSITION_X, MT_POSITION_Y, TRAKCING_ID, SLOT, TOUCH_MAJOR
+# MT_PRESSURE, MT_POSITION_X, MT_POSITION_Y, TRACKING_ID, SLOT, TOUCH_MAJOR
 # unsupported:
 # ABS_MT_TOUCH_MINOR, ABS_MT_WIDTH_MAJOR, ABS_MT_WIDTH_MINOR, ABS_MT_DISTANCE
 # ABS_MT_ORIENTATION, ABS_MT_TOOL_X, ABS_MT_TOOL_Y
@@ -20,89 +44,127 @@ class MultiTouchTypeBParser:
     def __init__(self):
         # states
         self.currentSlotIndex = 0
-        self.currentSlot = self.genNewSlot()
+        self.currentSlot = MotionEvent()
         self.slots = [self.currentSlot]
-    def genNewSlot(self):
-        return { "x" : 0, "y" : 0, "pressure" : 0, "tracking_id" : 0xFFFFFFFF, "touch_major" : 0}
-    def process(self, tp, reporter):
-        (timestamp, evType, evCmd, evVal) = tp
-        if evType == "EV_ABS":
-            if evCmd == "ABS_MT_SLOT":
-                self.currentSlotIndex = evVal
-                if evVal >= len(self.slots):
-                    self.slots.extend([None]*(evVal + 1 - len(self.slots)))
-                    self.slots[evVal] = self.genNewSlot()
+    def next(self, geteventCmd):
+        parcel = PipelineParcel()
+        if geteventCmd.evType == "EV_ABS":
+            if geteventCmd.evCmd == "ABS_MT_SLOT":
+                self.currentSlotIndex = geteventCmd.evVal
+                if geteventCmd.evVal >= len(self.slots):
+                    self.slots.extend([None]*(geteventCmd.evVal + 1 - len(self.slots)))
+                    self.slots[geteventCmd.evVal] = MotionEvent()
                 self.currentSlot = self.slots[self.currentSlotIndex]
-            elif evCmd == "ABS_MT_POSITION_X":
-                self.currentSlot["x"] = evVal
-            elif evCmd == "ABS_MT_POSITION_Y":
-                self.currentSlot["y"] = evVal
-            elif evCmd == "ABS_MT_TRACKING_ID":
-                if evVal == 0xFFFFFFFF:
+            elif geteventCmd.evCmd == "ABS_MT_POSITION_X":
+                self.currentSlot.x = geteventCmd.evVal
+            elif geteventCmd.evCmd == "ABS_MT_POSITION_Y":
+                self.currentSlot.y = geteventCmd.evVal
+            elif geteventCmd.evCmd == "ABS_MT_TRACKING_ID":
+                if geteventCmd.evVal == 0xFFFFFFFF:
                     # unbinding
-                    self.slots[self.currentSlotIndex] = self.genNewSlot()
+                    self.slots[self.currentSlotIndex] = MotionEvent()
                 else:
                     # binding tracking_id to slot
-                    self.currentSlot["tracking_id"] = evVal
-            elif evCmd == "ABS_MT_PRESSURE":
-                self.currentSlot["pressure"] = evVal
-            elif evCmd == "ABS_MT_TOUCH_MAJOR":
-                self.currentSlot["touch_major"] = evVal
+                    self.currentSlot.tracking_id = geteventCmd.evVal
+            elif geteventCmd.evCmd == "ABS_MT_PRESSURE":
+                self.currentSlot.pressure = geteventCmd.evVal
+            elif geteventCmd.evCmd == "ABS_MT_TOUCH_MAJOR":
+                self.currentSlot.touch_major = geteventCmd.evVal
             else:
-                print "[WARN] MTDriver meets unknown evCmd" + str(tp)
-        elif evType == "EV_SYN":
-            if evCmd == "SYN_REPORT":
-                for ev in self.slots:
-                    if ev["tracking_id"] != 0xFFFFFFFF:
-                        reporter.report(timestamp, # current timestamp
-                            ev["tracking_id"], # this specifies which finger
-                            ev["touch_major"], # the diameter of the touch
-                            ev["x"], # the X coordinate, in pixel
-                            ev["y"], # the Y coordinate , in pixel
-                            ev["pressure"]) # the pressure
+                print "[WARN] Type B MT meets unknown evCmd" + str(geteventCmd)
+        elif geteventCmd.evType == "EV_SYN":
+            if geteventCmd.evCmd == "SYN_REPORT":
+                for motionEvent in self.slots:
+                    if motionEvent.tracking_id != 0xFFFFFFFF:
+                        motionEvent.timestmp = geteventCmd.timestamp
+                        parcel.enqueue(motionEvent)
             else:
-                print "[WARN] MTDriver meets unknwon evCmd" + str(tp)
+                print "[WARN] Type B MT meets unknown evCmd" + str(geteventCmd)
         else:
-            print "[WARN] MTDriver skips unknown line:" + str(tp)
+            print "[WARN] Type B MT skips unknown line:" + str(geteventCmd)
+        return parcel
 
-# provide an interface like the MotionEvent in Android
-class MultiTouchRecorder:
-    def report(self, timstamp, tracking_id, touch_major, x, y, pressure):
-        tp = (timstamp, tracking_id, touch_major, x, y, pressure)
-        print tp
-    
-def processRawTrace(tracePath, myHandlers, evDev):
-    pattern = re.compile("\\[\s*(\d+\.\d+)\\]\s*(\w+)\s*(\w+)\s*(\w+)")
-    fp = open(tracePath)
-    for (i, line) in enumerate(fp):
+# A generic printer, print whatever given
+class GenericPrinter:
+    def next(self, whatever):
+        print str(whatever)
+        return PipelineParcel()
+
+# A text file reader which reads the file line by line
+class TextFileLineReader:
+    def __init__(self, tracePath):
+        self.fp = open(tracePath)
+    def next(self, dummy):
+        parcel = PipelineParcel()
+        line = self.fp.readline()
+        if line != "":
+            parcel.enqueue(line)
+        return parcel        
+
+# A trace parser for raw getevent trace
+class RawTraceParser:
+    def __init__(self):
         # a line is in the format of:
         # "time(float) evType(str) evCmd(str) evVal(int)"
         # refer to Linux evdev for details
         # here we assume the line is a readable dump from getevent
-        m = pattern.match(line)
-        timestamp = float(m.group(1))
-        evType = m.group(2)
-        evCmd = m.group(3)
-        evVal = int(m.group(4), 16)
-        tp = (timestamp, evType, evCmd, evVal)
-        # find the corresponding driver for this component
-        if evDev in myHandlers:
-            (driver, reporter) = myHandlers[evDev];
-            driver.process(tp, reporter)
+        self.pattern = re.compile("\\[\s*(\d+\.\d+)\\]\s*(\w+)\s*(\w+)\s*(\w+)")
+    def next(self, line):
+        m = self.pattern.match(line)
+        e = GeteventCommand()
+        e.timestamp = float(m.group(1))
+        e.evType = m.group(2)
+        e.evCmd = m.group(3)
+        e.evVal = int(m.group(4), 16)
+        parcel = PipelineParcel()
+        parcel.enqueue(e)
+        return parcel
+
+class FingerDecomposer:
+    def __init__(self):
+        self.tracker = {}
+    def next(self, motionEvent):
+        if motionEvent.tracking_id in self.tracker:
+            self.tracker[motionEvent.tracking_id].append(motionEvent)
         else:
-            print "[WARN] Unknown device: " + str(tp)
+            self.tracker[motionEvent.tracking_id] = [motionEvent]
+        return PipelineParcel()
+
+class Pipeline:
+    def __init__(self):
+        self.pl = []
+    def execute(self):
+        first = self.pl[0]
+        while True:
+            parcel = first.next(None)
+            if parcel.isEmpty():
+                break
+            while not parcel.isEmpty():
+                obj = parcel.dequeue()
+                self._executeSingleStep(1, obj)
+    def _executeSingleStep(self, index, obj):
+        if index >= len(self.pl):
+            return None
+        parcel = self.pl[index].next(obj)
+        while not parcel.isEmpty():
+            nextObj = parcel.dequeue()
+            self._executeSingleStep(index+1, nextObj)
+    def addStep(self, step):
+        self.pl.append(step)
 
 def main():
     if len(sys.argv) <= 1:
         print "Usage: python test.py TRACE_PATH"
-        print "The trace must be generated from getevent -lp [EVDEV]"
+        print "The trace must be generated from getevent -lt [EVDEV]"
         return 1
-
-    # TODO: now hardcoded, a mapping from a evDev to its handlers
-    # for each device, we need one parser and one reporter
-    myHandlers = {"/dev/input1" : (MultiTouchTypeBParser(), MultiTouchRecorder())}
     print ("timestamp", "tracking_id", "touch_major", "x", "y", "pressure")
-    processRawTrace(sys.argv[1], myHandlers, "/dev/input1")
+    pl = Pipeline()
+    pl.addStep(TextFileLineReader(sys.argv[1]))
+    pl.addStep(RawTraceParser())
+    pl.addStep(MultiTouchTypeBParser())
+    # pl.addStep(FingerDecomposer())
+    pl.addStep(GenericPrinter())
+    pl.execute()
 
 if __name__ == "__main__":
     main()
