919351b39124904a0edd5a3d13f443c80174d014
Mingyuan Xia <ken.mingyuan@gmail.com>
2014-07-18 15:58:28 -0700

    add apache license header

--- src/test.py
+++ src/test.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python
+#
+# Copyright 2014 Mingyuan Xia (http://mxia.me) and others
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+# Contributors:
+#   Mingyuan Xia
+#   Ran Shu
+#
+
 import os, sys, inspect
 def module_path():
     ''' returns the module path without the use of __file__.
