d82dad34f50c7007380e139df037ce89e96ee62c
Mingyuan Xia <ken.mingyuan@gmail.com>
2014-07-20 19:46:42 -0700

    AutoScroller finished, with some hard-coded paths

--- src/AutoScroller.py
+++ src/AutoScroller.py
@@ -29,21 +29,84 @@ def module_path():
     return os.path.abspath(os.path.dirname(inspect.getsourcefile(module_path)))
 sys.path.append(module_path())
 
-from Pipeline import Pipeline
+from Pipeline import Pipeline, PipelineParcel
 import TraceManipulation as dtm
-from MonkeyHelperReplayer import MonkeyHelperReplayer
-from MonkeyHelper import MonkeyHelper, EMonkeyDevice, _cmd
-import subprocess
+from MonkeyHelper import EMonkeyDevice
+import subprocess, signal
 
+class LiveGeteventReader:
+    def __init__(self):
+        cmd = '/Applications/android-sdk-macosx/platform-tools/adb shell getevent -lt /dev/input/event1'
+        self.p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        self.mustTerminate = False
+    def next(self, dummy):
+        parcel = PipelineParcel()
+        line = self.p.stdout.readline()
+        if self.mustTerminate or (line == '' and self.p.poll() != None):
+            return parcel
+        parcel.enqueue(line)
+        return parcel
+    def terminate(self):
+        # not available on Jython 2.53 (with monkeyrunner of Android SDK)
+        # self.p.terminate()
+        # has to do this, with a process leak
+        self.mustTerminate = True
+
+class AutoScrollingLearner:
+    def __init__(self, reader, sample_count):
+        self.reader = reader
+        self.sample_count = sample_count
+        self.samples = []
+        self.lastTimestamp = 0
+        self.stats = None
+    def next(self, trail):
+        pointCount = len(trail)
+        duration = trail[len(trail)-1].timestamp - trail[0].timestamp
+        xdelta = trail[len(trail)-1].x - trail[0].x
+        ydelta = trail[len(trail)-1].y - trail[0].y
+        waitTime = trail[0].timestamp - self.lastTimestamp
+        self.lastTimestamp = trail[len(trail)-1].timestamp
+        sample = (waitTime, xdelta, ydelta, duration, pointCount)
+        self.samples.append(sample)
+        print sample
+        self.sample_count -= 1 
+        if self.sample_count == 0: 
+            print "learning finished, do statistics"
+            self.reader.terminate()
+            count = len(self.samples)
+            total = (0, 0, 0, 0, 0)
+            for (_1, _2, _3, _4, _5) in self.samples:
+                total = (total[0] + _1, total[1] + _2, total[2] + _3, total[3] + _4, total[4] + _5)
+            self.stats = (total[0] / count, total[1] / count, total[2] / count, total[3] / count, total[4] / count)
+            print self.stats
+        return PipelineParcel()
+    def getSpeedAndDelta(self):
+        return self.stats
+    
 def main():
+    # capture three samples and automatically scrolls five times
+    SAMPLE_COUNT = 3
+    REPEAT_COUNT = 5
     print "learning the pace of scrolling"
+    pl = Pipeline()
+    reader = LiveGeteventReader()
+    pl.addStep(reader)
+    pl.addStep(dtm.RawTraceParser())
+    pl.addStep(dtm.MultiTouchTypeAParser())
+    pl.addStep(dtm.RelativeTimingConverter())
+    pl.addStep(dtm.FingerDecomposer())
+    learner = AutoScrollingLearner(reader, SAMPLE_COUNT)
+    pl.addStep(learner)
+    pl.execute()
+    (waitTime, xdelta, ydelta, duration, pointCount) = learner.getSpeedAndDelta()
+    print "User scrolling parameters learned"
     device = EMonkeyDevice()
-    # cmd = ["adb", "shell", "getevent", "-lt"]
-    cmd = ["adb", "shell", "logcat"]
-    p = subprocess.Popen(cmd, stdout = subprocess.PIPE, 
-                stderr = subprocess.PIPE)
-    for line in p.stdout:
-        print line
-        
+    (xmiddle, ymiddle) = (device.displayWidth / 2, device.displayHeight / 2)
+    start = (xmiddle - xdelta/2, ymiddle - ydelta/2)
+    end = (xmiddle + xdelta/2, ymiddle + ydelta/2)
+    for _ in range(REPEAT_COUNT):
+        device.sleep(waitTime)
+        device.drag(start, end, duration, pointCount)
+ 
 if __name__ == "__main__":
     main()
