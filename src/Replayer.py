40a84a6f344fbd8e36d6edb77186ac235fe3446e
rshu88 <ran.shu212@gmail.com>
2014-07-26 02:00:45 -0400

    Update Replayer.py

--- src/Replayer.py
+++ src/Replayer.py
@@ -46,10 +46,10 @@ def main():
     pl.addStep(dtm.RelativeTimingConverter())
     pl.addStep(dtm.FingerDecomposer())
     # this step might be necessary for a tablet
-    # pl.addStep(TrailScaler(0.8))
+    # pl.addStep(dtm.TrailScaler(0.8))
     # pl.addStep(dtm.TimeScaler(0.25))
     pl.addStep(MonkeyHelperReplayer())
-    # pl.addStep(GenericPrinter())
+    # pl.addStep(dtm.GenericPrinter())
     pl.execute()
     print "Replay finished"
         
