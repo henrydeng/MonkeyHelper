42c101e10ec55f290ced9a3a66b697460fea3539
rshu88 <ran.shu212@gmail.com>
2014-08-25 02:58:52 -0400

    Update SnapshortAgentTest.py

--- src/SnapshortAgentTest.py
+++ src/SnapshortAgentTest.py
@@ -13,5 +13,8 @@ def main():
     print "working 5"
     print tools.takeAndCompareSnapshots(tools.loadSnapshot('screen_shot2.png'))
     print "working 6"
+    savedImage2 = tools.getSubSnapshot(tools.loadSnapshot('screen_shot2.png'), (100,100,100,100))
+    tools.saveSnapshot(savedImage2, 'tempPic2')
+    print "working 7"
 if __name__ == "__main__":
     main()
